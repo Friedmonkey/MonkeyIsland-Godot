ff46277b1bfb1346f173d15d46bc3f9a9b29bca2
FriedMonkey <120042050+Friedmonkey@users.noreply.github.com>
2025-05-22 21:34:05 +0200

    added custom FriedGrass plugin

--- addons/terrain_3d/extras/import_sgt.gd
+++ addons/terrain_3d/extras/import_sgt.gd
@@ -1,12 +1,12 @@
 # Copyright © 2025 Cory Petkovsek, Roope Palmroos, and Contributors.
-# Import From SimpleGrassTextured
+# Import From FriedGrass
 # 
-# This script demonstrates how to import transforms from SimpleGrassTextured. To use it:
+# This script demonstrates how to import transforms from FriedGrass. To use it:
 #
 # 1. Setup the mesh asset you wish to use in the asset dock.
 # 1. Select your Terrain3D node.
 # 1. In the inspector, click Script (very bottom) and Quick Load import_sgt.gd.
-# 1. At the very top, assign your SimpleGrassTextured node.
+# 1. At the very top, assign your FriedGrass node.
 # 1. Input the desired mesh asset ID.
 # 1. Click import. The output window and console will report when finished.
 # 1. Clear the script from your Terrain3D node, and save your scene. 
@@ -22,7 +22,7 @@
 @tool
 extends Terrain3D
 
-@export var simple_grass_textured: MultiMeshInstance3D
+@export var fried_grass: MultiMeshInstance3D
 @export var assign_mesh_id: int
 @export var import: bool = false : set = import_sgt
 @export var clear_instances: bool = false : set = clear_multimeshes
@@ -33,10 +33,10 @@ func clear_multimeshes(value: bool) -> void:
 
 
 func import_sgt(value: bool) -> void:
-	var sgt_mm: MultiMesh = simple_grass_textured.multimesh
-	var global_xform: Transform3D = simple_grass_textured.global_transform	
-	print("Starting to import %d instances from SimpleGrassTextured using mesh id %d" % [ sgt_mm.instance_count, assign_mesh_id])
+	var sgt_mm: MultiMesh = fried_grass.multimesh
+	var global_xform: Transform3D = fried_grass.global_transform	
+	print("Starting to import %d instances from FriedGrass using mesh id %d" % [ sgt_mm.instance_count, assign_mesh_id])
 	var time: int = Time.get_ticks_msec()
-	get_instancer().add_multimesh(assign_mesh_id, sgt_mm, simple_grass_textured.global_transform)	
+	get_instancer().add_multimesh(assign_mesh_id, sgt_mm, fried_grass.global_transform)	
 	print("Import complete in %.2f seconds" % [ float(Time.get_ticks_msec() - time)/1000. ])
 	

ee618776742a378cfa9fb16c8153e50257d22276
FriedMonkey <120042050+Friedmonkey@users.noreply.github.com>
2025-05-30 18:19:57 +0200

    added grass ambiance

--- background_music.gd
+++ background_music.gd
@@ -2,11 +2,18 @@ extends Node
 
 @onready var music: AudioStreamPlayer = $Music
 @onready var waves: AudioStreamPlayer = $Waves
+@onready var grass: AudioStreamPlayer = $Grass
 
 @export var tracks: Array[AudioStream]  # ensure it's audio streams!
 @export var player: Node3D
 
 var current_bag: Array = []
+
+var waves_target_volume_db := -80.0  # Target dB we want to fade to
+var waves_current_volume_db := -80.0  # Current interpolated dB
+
+var grass_target_volume_db := -80.0  # Target dB we want to fade to
+var grass_current_volume_db := -80.0  # Current interpolated dB
 
 func _ready():
 	prepare_new_bag()
@@ -14,7 +21,7 @@ func _ready():
 	music.connect("finished", Callable(self, "play_next_track"))
 	
 func _process(delta: float) -> void:
-	handle_terrain_meta()
+	handle_terrain_meta(delta)
 
 func prepare_new_bag():
 	current_bag = tracks.duplicate()
@@ -33,12 +40,26 @@ func volume_from_factor(factor):
 	var max_db = 24
 	return lerp(min_db, max_db, factor)
 
-func handle_terrain_meta():
-	var factor = get_terrain_meta("Water")
-	var volume_db = volume_from_factor(factor)
-	waves.volume_db = volume_db
+#func handle_terrain_meta():
+	#var factor = get_terrain_meta("Water")
+	#var volume_db = volume_from_factor(factor)
+	#waves.volume_db = volume_db
+	#
+	#pass 
 	
-	pass 
+
+func handle_terrain_meta(delta: float) -> void:
+	var water_factor = get_terrain_meta("Water")
+	waves_target_volume_db = volume_from_factor(water_factor)
+	waves_current_volume_db = lerp(waves_current_volume_db, waves_target_volume_db, delta * 5.0)
+	waves.volume_db = waves_current_volume_db
+	
+	var grass_factor = get_terrain_meta("Grass")
+	grass_target_volume_db = volume_from_factor(grass_factor)
+	grass_current_volume_db = lerp(grass_current_volume_db, grass_target_volume_db, delta * 5.0)
+	grass.volume_db = grass_current_volume_db
+	
+	# Smooth interpolation (tweak 5.0 to change speed)
 func get_terrain_meta(meta : String) -> float:
 	var pos = player.global_position
 	var info = %TerrainMeta.GetPositionInformation(pos.x, pos.z)
